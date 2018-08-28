23bbd129d1bd66d925baad55533108d8c3b7d2fb
Brandon Russell <rbrandon87@gmail.com>
2018-08-28 13:32:46 -0400

    Backend done
    Backend done

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -26,6 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	  @Override
 	  protected void configure(HttpSecurity http) throws Exception {
+		  
+		// Add this row to allow access to all endpoints
+		//  http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
+		  	  
 		 http.csrf().disable().cors().and().authorizeRequests()
 		  .antMatchers(HttpMethod.POST, "/login").permitAll()
 	        .anyRequest().authenticated()
@@ -37,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	        .addFilterBefore(new AuthenticationFilter(),
 	                UsernamePasswordAuthenticationFilter.class);
 	  }
-  
+	  
 	  @Bean
 	  CorsConfigurationSource corsConfigurationSource() {
 	      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
